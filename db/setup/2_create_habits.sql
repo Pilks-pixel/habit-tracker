ee110add8f725b2fbd9305444d20cf98a799e93d
NoamaanMohamed <noamaan.mohamed@hotmail.co.uk>
2022-01-18 10:33:20 +0000

    stuff

--- db/setup/2_create_habits.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS habits;
-
-CREATE TABLE habits (
-    habit_id serial PRIMARY KEY,
-    habit_name varchar(100) NOT NULL
-    );
--- db/setup/4_create_habits_plan.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS habits_facts;
-
-CREATE TABLE habits_facts (
-    hfact_id INT,
-    hplan_id INT,
-    hfact_timestamp TIMESTAMP
-    );

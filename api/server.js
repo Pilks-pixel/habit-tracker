c43599fcd247591affcb0a99a314cf71f9002938
NoamaanMohamed <noamaan.mohamed@hotmail.co.uk>
2022-01-18 15:24:43 +0000

    new files

--- api/server.js
+++ api/server.js
@@ -11,10 +11,12 @@ app.use(express.json());
 const authRoutes = require('./controllers/auth');
 const userRoutes = require('./controllers/users');
 const habitRoutes = require('./controllers/habits')
+const habitPlansRoutes = require('./controllers/habit_plan')
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/habits', habitRoutes);
+app.use('/habitplans', habitPlansRoutes);
 
 
 app.get('/', (req, res) => res.json({ message: 'Welcome' }));
